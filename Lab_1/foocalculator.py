b2bb4ca544fad539446e141824249711f76412de
Federico Silvestri <federico.silvestri@vedrai.com>
2020-09-18 10:35:47 +0200

    Added class

---
+++ Lab_1/foocalculator.py
@@ -0,0 +1,13 @@
+# calc_class.py
+
+import calculator as c
+
+class FooCalculator:
+    def __init__(self):
+        pass
+
+    def sum(self, m, n):
+        return c.sum(m, n)
+
+    def divide(self, m, n):
+        return c.divide(m, n)
